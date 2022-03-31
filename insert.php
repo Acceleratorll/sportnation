560172d06bb209b04efdf4072be37558d49551d8
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-31 16:21:53 +0700

    coba insert #4
    Membuat fungsi insert mengecek apabila data sudah ada atau belum, apabila data sudah ada maka akan ada alert "Data sudah ada!" dan apabila belum ada maka data akan ditambahkan ke database dan ada alert "Data berhasil ditambahkan!"

--- insert.php
+++ insert.php
@@ -4,17 +4,37 @@
     //functions
     function tambahData($connect, $kota_id, $nama_kota) {
         $sql = 
-            "INSERT INTO kota
-             VALUES ('$kota_id', '$nama_kota');";
+            "SELECT * 
+             from kota 
+             WHERE kota_id = '$kota_id'";
         $result = mysqli_query($connect, $sql);
+        $rowCount = mysqli_num_rows($result);
 
-        alertTambahData();
+        echo $rowCount;
+
+        if($rowCount == 0) {
+            $sql = 
+                "INSERT INTO kota
+                VALUES ('$kota_id', '$nama_kota');";
+            $result = mysqli_query($connect, $sql);
+
+            alertTambahData();
+        } else {
+            alertGagalTambahData();
+        }
     }
 
     function alertTambahData() {
         echo 
             '<script type="text/javascript">
                 alert("Data berhasil ditambahkan!"); 
+            </script>';
+    }
+
+    function alertGagalTambahData() {
+        echo 
+            '<script type="text/javascript">
+                alert("Data sudah ada!"); 
             </script>';
     }
 
@@ -31,10 +51,10 @@
     echo "Jumlah data saat ini: " . hitungJumlahData($connect);
     echo "<br>";
 
-    //tambah data
+    //tambah data 
     tambahData($connect ,"03K", "Batu");
 
-    //menghitung jumlah baris awal
+    //menghitung jumlah baris setelah ditambahkan
     echo "Jumlah data setelah ditambahkan: " . hitungJumlahData($connect);
     echo "<br>";
 ?>
